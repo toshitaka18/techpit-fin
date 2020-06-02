6aa287373b7da2e366e171aefc0a1f9f7ec6d931
toshitaka18 <toshitaka0018@gmail.com>
2020-06-02 22:44:22 +0900

    humanize

--- techpit/settings.py
+++ techpit/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'finchart',
+    'django.contrib.humanize',
 ]
+
+NUMBER_GROUPING = 3
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
